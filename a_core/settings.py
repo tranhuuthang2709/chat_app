b09b73b08e4fbf07c29ae47c45c4ba5b919f110a
tranhuuthang2709 <112497755+tranhuuthang2709@users.noreply.github.com>
2024-12-27 21:16:44 +0700

    upload

--- a_core/settings.py
+++ a_core/settings.py
@@ -33,6 +33,7 @@ CSRF_TRUSTED_ORIGINS = [ 'https://*' ]
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
     'django_htmx',
     'a_home',
     'a_users',
+    'a_rtchat',
 ]
 
 SITE_ID = 1
@@ -85,9 +87,21 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'a_core.wsgi.application'
-
-
+# WSGI_APPLICATION = 'a_core.wsgi.application'
+ASGI_APPLICATION = 'a_core.asgi.application'
+CHANNEL_LAYERS = {
+        'default': {
+            "BACKEND": "channels.layers.InMemoryChannelLayer",
+        }
+    }   
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels_redis.core.RedisChannelLayer",
+#         "CONFIG": {
+#             "hosts": [('redis://default:kRLNYnmGqByRuxSASCBPirdyLJefcgOc@autorack.proxy.rlwy.net:46213')],
+#         },
+#     },
+# }
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
---
+++ a_rtchat/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', chat_view, name="home"),
+    path('chat/<username>', get_or_create_chatroom, name="start-chat"),
+    path('chat/room/<chatroom_name>', chat_view, name="chatroom"),
+    path('chat/new_groupchat/', create_groupchat, name="new-groupchat"),
+    path('chat/edit/<chatroom_name>', chatroom_edit_view, name="edit-chatroom"),
+    path('chat/delete/<chatroom_name>', chatroom_delete_view, name="chatroom-delete")
+]
